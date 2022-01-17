c2b6a7b2f353a00182b3b72e4d1cba09e3d1eb67
Apollos1301 <88384358+Apollos1301@users.noreply.github.com>
2022-01-17 12:11:05 +0100

    amazon finish

--- crawler/main.py
+++ crawler/main.py
@@ -1,16 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url = "https://www.amazon.de/s?k=schmuck+damen&sprefix=Schmuck+%2Caps%2C74&ref=nb_sb_ss_ts-doa-p_1_8"
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'}
-
-
-source_code = requests.get(url, headers=headers)
-soup = BeautifulSoup(source_code.content, 'html.parser')
-for numm in soup.findAll("div",{"class":"a-row a-size-base a-color-secondary"}):
-    print(numm.h5)
-#for link in soup.findAll('div', {'class': 'grid-product__title'}):
-#    title = link.text
-#    print(title)
-# sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20
+class ObjProducts:
+    def __init__(self,id, link, name, image, price, rating):
+        self.id = id
+        self.link = link
+        self.name = name
+        self.image = image
+        self.price = price
+        self.rating = rating
+        
+def amaz_damen():
+    urlHome = "https://www.amazon.de/s?k=damen+schmuck&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3GVTPT1NU38C8&sprefix=damen+schmuck%2Caps%2C130&ref=nb_sb_noss"
+    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'}
+    soupHome = BeautifulSoup(requests.get(urlHome, headers=headers).content, 'html.parser')
+    nextPage = soupHome.findAll("li",{"class":"a-normal"})
+    products = []
+    for numm in soupHome.findAll("a",{"class":"a-link-normal s-no-outline"}):
+            products.append(numm.get("href").strip())
+    page = 0
+    while page <=0:
+        url = "https://www.amazon.de/"+str(nextPage[page].a.get("href"))
+        soup = BeautifulSoup(requests.get(url, headers=headers).content,'html.parser')
+        for numm in soup.findAll("a",{"class":"a-link-normal s-no-outline"}):
+            products.append(numm.get("href").strip())
+        page += 1
+        
+    products= list(dict.fromkeys(products))
+    productInfo = []
+    productID = 0
+    counter = 0
+    for link in products:
+        productURL = "https://www.amazon.de/"+str(link)
+        productSoup = BeautifulSoup(requests.get(productURL, headers=headers).content,'html.parser')
+        productName = productSoup.find("span",{"id":"productTitle"}).text.strip()
+        productImage = productSoup.find("img",{"id":"landingImage"}).get("src").strip()
+        productPrice = productSoup.find("span",{"class":"a-price"}).span.text.strip()
+        productRating = productSoup.find("span",{"class":"a-icon-alt"}).text[0:3].strip()
+        productInfo.append(ObjProducts(productID, productURL, productName, productImage, productPrice, productRating))
+        productID += 1
+        #print(productInfo[counter].price +" ====>   "+ productInfo[counter].link)
+        print(" ")
+        print(" ")
+        counter += 1
+amaz_damen()
